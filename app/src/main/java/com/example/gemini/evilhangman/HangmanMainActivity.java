aa70a2cf330e022897ee267cec56b446dee2a86e
Tom Pelsmaeker <tlpelsmaeker@gmail.com>
2015-11-06 21:54:13 +0100

    First commit

---
+++ app/src/main/java/com/example/gemini/evilhangman/HangmanMainActivity.java
@@ -0,0 +1,13 @@
+package com.example.gemini.evilhangman;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class HangmanMainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hangman_main);
+    }
+}
